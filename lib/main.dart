2ec62a94f341b54ee827769af40db9270af26a69
DRohan007 <drohan940@gmail.com>
2020-08-27 21:39:33 +0530

    Pdf_Maker[wip]: Add Pdf from images logic *Works for single images for now
    Signed-off-by: DRohan007 <drohan940@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf_flutter/pdf_flutter.dart';
+import 'package:pdf/widgets.dart' as pw;
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main(){
@@ -23,7 +26,32 @@ class Home_Page extends StatefulWidget {
 }
 
 class _Home_PageState extends State<Home_Page> {
+  final pdf = pw.Document();
   File file;
+  void generate_pdf() async {
+  File temp = await FilePicker.getFile(
+    type: FileType.custom,
+    allowedExtensions: ['jpeg'],
+              //  allowedExtensions: ['pdf'],
+              );
+  
+  final image = PdfImage.file(
+  pdf.document,
+  bytes: temp.readAsBytesSync(),
+);
+
+pdf.addPage(pw.Page(
+    build: (pw.Context context) {
+      return pw.Center(
+        child: pw.Image(image),
+      );// Center
+    }));
+    //write to file
+    final op = await getExternalStorageDirectory();
+    String pathtowrite = op.path + '/test2.pdf';
+    File op_file = File(pathtowrite);
+    await op_file.writeAsBytesSync(pdf.save());
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +87,7 @@ class _Home_PageState extends State<Home_Page> {
             label: "Create Pdf",
             child: Icon(Icons.create_new_folder),
             onTap: () {
-              
+              generate_pdf();
             },
           ),
         ],
