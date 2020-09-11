a9ae71afdd9365d4e58dc37325067885743469da
DRohan007 <drohan940@gmail.com>
2020-09-11 14:36:49 +0530

    Pdf_Maker: Import plugins & add pdf page
    Signed-off-by: DRohan007 <drohan940@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,86 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'createpdf.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'pdf_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(pdf_maker());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class pdf_maker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/pdfpage': (context) => pdf_create(),
+      },
+      debugShowCheckedModeBanner: false,
+      home: Home_Page(),      
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home_Page extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _Home_PageState createState() => _Home_PageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _Home_PageState extends State<Home_Page> {
+  File file;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Pdf Maker"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+             Center(
+                child: Text("Pdf Maker"),
+                   ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      // IconButton(
+      //   onPressed: (){},
+      //   icon: Icons.create_new_folder,
+      // ),
+      floatingActionButton: SpeedDial(
+        tooltip: "Tap",
+        child: Icon(Icons.menu),
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.picture_as_pdf),
+            label: 'Open Pdf',
+            onTap: () async {
+              File picked_file = await FilePicker.getFile(
+                type: FileType.custom,
+                allowedExtensions: ['pdf'],
+              );
+              setState(() {
+                file = picked_file;
+              });
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => pdf_show(file),
+                ),
+                );
+            },
+          ),
+          SpeedDialChild(
+            label: "Create Pdf",
+            child: Icon(Icons.create_new_folder),
+            onTap: () {
+                Navigator.pushNamed(context, '/pdfpage');
+            },
+          ),
+        ],
+      ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
+
+//
